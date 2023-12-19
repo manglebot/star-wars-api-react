3f23c2fd995800e4e7633d6f0318e3085e04e021
Ben <manglebot@users.noreply.github.com>
2023-12-19 20:03:08 +0000

    fix:add server mocks

--- src/components/use_fetch/use_fetch.test.ts
+++
@@ -1,6 +0,0 @@
-import { useFetch } from "./use_fetch";
-
-describe("test use fetch", () => {
-    useFetch
-  expect(null).toBe(null);
-});
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,18 +1,7 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
 
-import { setupServer } from "msw/node";
-import { handlers } from "./mocks/handlers";
-
-const server = setupServer(...handlers);
+import { server } from "./mocks/server";
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
-
-// Ensure that Jest doesn't throw an error for unresolved promises
-// when MSW captures requests that don't have mocks.
-beforeEach(() => jest.spyOn(console, "error").mockImplementation());
